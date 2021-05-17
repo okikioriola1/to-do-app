daa2b68d8edc4496e9611a50eac635cd432e1b10
Okiki Oriola <okikioriola1@gmail.com>
2021-05-17 16:11:48 +0100

    css changes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 
 import './App.css';
-import TodoForm from './components/TodoForm';
+
 import TodoList from './components/TodoList';
 import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="todo-app">
-     {/* <TodoForm/> */}
+     
      <TodoList/>
      <Footer/>
     </div>
